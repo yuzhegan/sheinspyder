54e467fe1b40d858cd78746b960b349e563c3afe
yuzhegan <boflyyang@163.com>
2021-12-28 04:58:57 -0800

    add change_language class and success to write data

---
+++ Change_language.py
@@ -0,0 +1,65 @@
+# encoding='utf-8
+
+# @Time: 2021-12-28
+# @File: %
+#!/usr/bin/env
+from icecream import ic
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+import time
+
+
+
+class Change_language():
+
+    """Docstring for Change_language. """
+
+    def load(self, brower, url):
+        if brower != None and url != "":
+
+            brower.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+                "source": """
+                Object.defineProperty(navigator, 'webdriver', {
+                  get: () => undefined
+                })
+              """
+
+            })
+            brower.get(url)
+            time.sleep(2)
+
+            # 等待弹窗, 点击关闭按钮
+            try:
+                WebDriverWait(brower, 10).until(EC.presence_of_element_located(
+                  (By.CLASS_NAME, "c-coupon-box")))
+                brower.find_element_by_xpath("/html/body/div[1]/div[3]/div[1]/div/i").click()
+                time.sleep(3)
+            except Exception as e:
+                print(e)
+
+            # 切换语言为日语 点击语言列表
+            try:
+                # WebDriverWait(brower,10).until(EC.presence_of_element_located(
+                    # (By.CLASS_NAME, "iconfont-critical icon-head-global")))
+                brower.find_element_by_xpath("/html/body/div[1]/header/div[2]/div[1]/div/div[1]/div/div[3]/div[5]/a/span/i").click()
+                time.sleep(5)
+
+            except Exception as e:
+                print(e)
+            # 切换语言为日语
+
+            try:
+                # WebDriverWait(brower, 10).until(EC.presence_of_element_located(
+                  # (By.CLASS_NAME, "col-xs-4 j-change-language")))
+                brower.find_element_by_xpath("/html/body/div[1]/header/div[2]/div[1]/div/div[1]/div/div[3]/div[5]/div/div[4]/a[2]").click()
+                time.sleep(5)
+            except Exception as e:
+                print(e)
+
+
+# if __name__ =="__main__":
+    # url = "https://jp.shein.com/DAZY-Solid-Cable-Knit-Drop-Shoulder-Sweater-p-5835597-cat-1734.html?scici=WomenHomePage~~ON_Banner,CN_category,HZ_Knitwear,HI_hotZoneuadal3wddc8~~6_1~~real_2216~~~~"
+    # a = Change_language()
+    # a.load(url)
--- get_detail.py
+++ get_detail.py
@@ -15,58 +15,58 @@ import json
 import time
 # from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.wait import WebDriverWait
-url = "https://jp.shein.com/Honeyspot-Solid-Fluffy-Sweater-p-7416907-cat-1734.html?scici=WomenHomePage~~ON_Banner,CN_category,HZ_Knitwear,HI_hotZoneuadal3wddc8~~6_1~~real_2216~~~~"
+# url = "https://jp.shein.com/DAZY-Solid-Cable-Knit-Drop-Shoulder-Sweater-p-5835597-cat-1734.html?scici=WomenHomePage~~ON_Banner,CN_category,HZ_Knitwear,HI_hotZoneuadal3wddc8~~6_1~~real_2216~~~~"
 
 
-path = "/home/dav/Downloads/chromedriver" 
-# %%
-# 关闭弹窗 模拟登录, 登录之后没有弹窗
+# path = "/home/dav/Downloads/chromedriver" 
+# # %%
+# # 关闭弹窗 模拟登录, 登录之后没有弹窗
 
-brower = webdriver.Chrome(executable_path=path)
-# 手动登录获取Sheincookies
-# 加载获取的Cookies登录网站
+# brower = webdriver.Chrome(executable_path=path)
+# # 手动登录获取Sheincookies
+# # 加载获取的Cookies登录网站
 
-# %%
-# def Get_DetilPage(url):
-# 通过网页地址打开网页，此时会弹出浏览器，并加载相应的网页
-brower.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
-    "source": """
-    Object.defineProperty(navigator, 'webdriver', {
-      get: () => undefined
-    })
-  """
+# # %%
+# # def Get_DetilPage(url):
+# # 通过网页地址打开网页，此时会弹出浏览器，并加载相应的网页
+# brower.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+    # "source": """
+    # Object.defineProperty(navigator, 'webdriver', {
+      # get: () => undefined
+    # })
+  # """
 
-})
-brower.get(url)
-time.sleep(2)
+# })
+# brower.get(url)
+# time.sleep(2)
 
-# 等待弹窗, 点击关闭按钮
-try:
-    WebDriverWait(brower, 10).until(EC.presence_of_element_located(
-      (By.CLASS_NAME, "c-coupon-box")))
-    brower.find_element_by_xpath("/html/body/div[1]/div[3]/div[1]/div/i").click()
-    time.sleep(3)
-except Exception as e:
-    print(e)
-
-# 切换语言为日语 点击语言列表
-try:
-    # WebDriverWait(brower,10).until(EC.presence_of_element_located(
-        # (By.CLASS_NAME, "iconfont-critical icon-head-global")))
-    brower.find_element_by_xpath("/html/body/div[1]/header/div[2]/div[1]/div/div[1]/div/div[3]/div[5]/a/span/i").click()
-    time.sleep(5)
-
-except Exception as e:
-    print(e)
-# 切换语言为日语
-
-try:
+# # 等待弹窗, 点击关闭按钮
+# try:
     # WebDriverWait(brower, 10).until(EC.presence_of_element_located(
-      # (By.CLASS_NAME, "col-xs-4 j-change-language")))
-    brower.find_element_by_xpath("/html/body/div[1]/header/div[2]/div[1]/div/div[1]/div/div[3]/div[5]/div/div[4]/a[2]").click()
-    time.sleep(5)
-except Exception as e:
-    print(e)
+      # (By.CLASS_NAME, "c-coupon-box")))
+    # brower.find_element_by_xpath("/html/body/div[1]/div[3]/div[1]/div/i").click()
+    # time.sleep(3)
+# except Exception as e:
+    # print(e)
+
+# # 切换语言为日语 点击语言列表
+# try:
+    # # WebDriverWait(brower,10).until(EC.presence_of_element_located(
+        # # (By.CLASS_NAME, "iconfont-critical icon-head-global")))
+    # brower.find_element_by_xpath("/html/body/div[1]/header/div[2]/div[1]/div/div[1]/div/div[3]/div[5]/a/span/i").click()
+    # time.sleep(5)
+
+# except Exception as e:
+    # print(e)
+# # 切换语言为日语
+
+# try:
+    # # WebDriverWait(brower, 10).until(EC.presence_of_element_located(
+      # # (By.CLASS_NAME, "col-xs-4 j-change-language")))
+    # brower.find_element_by_xpath("/html/body/div[1]/header/div[2]/div[1]/div/div[1]/div/div[3]/div[5]/div/div[4]/a[2]").click()
+    # time.sleep(5)
+# except Exception as e:
+    # print(e)
 
 
 
@@ -74,25 +74,22 @@ except Exception as e:
 # js="var q=document.documentElement.scrollTop=10000"  # 滚动到最下面
 # brower.execute_script(js)
 #######      shein 是已颜色为一个SKU的, 颜色下面分尺码, sku1-s, sku2-s
-soup = BeautifulSoup(brower.page_source, "lxml")
+
+
 # %%
 def Get_sku(soup):
     sku_doc = soup.find("div", {"class":"product-intro__head-sku-ctn"})
     sku = sku_doc.text.strip()
     # 'SKU: sw2109223427331015 (3051 Reviews)'
     sku = sku.split("(")[0][5:].strip()
-    ic(sku)
     return sku
-# ic(Get_sku(soup))
 
 # %%
 #pirce
 def Get_price(soup):
     price_doc = soup.find("div", {"class":"original"})
     price = price_doc.text.strip()
-    ic(price)
     return price
-# ic(Get_price(soup))
 
 
 # %%
@@ -101,7 +98,6 @@ def Get_brand(soup):
     brand_doc = soup.find("div", {"class":"product-intro__brand-name"})
     brand = brand_doc.text.strip()
     return brand
-    # ic(brand)
 # %%
 # 重构title 品牌都大些情况, DAZY 标题 Dazy-Less
 def Get_new_title(soup,brand):
@@ -110,7 +106,6 @@ def Get_new_title(soup,brand):
     # 因为日文转换大些不影响,所有可以标题都转换大些
     title = title_n.upper().replace(brand.upper(), "").strip()
     return title
-    # ic(title)
 
 
 # %%
@@ -132,7 +127,6 @@ def Get_colors_not_duplict(soup):
                     break
             colors.append(color2)
     return colors
-    # ic(colors)
 # %%
 # 获取尺码名称
 def Get_size_name(soup):
@@ -141,7 +135,6 @@ def Get_size_name(soup):
     for item in size_doc:
         sizes.append(item.text.strip())
     return sizes
-    # ic(sizes)
 
 # %%
 # 获取描述,详情
@@ -154,9 +147,6 @@ def Get_desc(soup):
         data = {"key":key, "value":value} 
         descs.append(data)
     return descs
-    # ic(descs)
-    # ic(descs[0]["key"])
-
 
 # %%
 #  转字符串存储, "|"分割表示
@@ -167,17 +157,13 @@ def Get_Desc_str(descs):
         str_descs +=  priex
     str_descs = str_descs.strip("|") #最后那个|要删除
     return str_descs
-    # ic(str_descs)
 
 # %%
 # 字符串转字典格式
 def Get_Desc_dict(str_descs):
     dect_list = str_descs.split("|")
     dect_list = [eval(item) for item in dect_list]
-    # ic(dect_list)
-    # ic(dect_list[0]["key"])
     return dect_list
-
 
 # %%
 # 转换成描述内容 字符串格式 亚马逊描述的html格式
@@ -186,8 +172,8 @@ def Get_Amz_desc(descs):
     for desc in descs:
         str_dec = "<b>" + desc["key"] + ":</b>" + "&nbsp;&nbsp;&nbsp;" + desc["value"] + "</br>"
         Str_desc += str_dec
-    # ic(Str_desc)
     return Str_desc
+
 
 
 def add_space(n):
@@ -210,8 +196,6 @@ def Get_Size_Chart(soup):
             row_datas.append(data.text.strip())
         rows.append(row_datas)
 
-    # ic(rows)
-    # %%
     # 放在亚马逊描述中,做成html表格形式 亚马逊不支持table标签
     # &nbsp; 表示不换行空格
     tables = ""
@@ -232,8 +216,6 @@ def Get_Size_Chart(soup):
                 tables += td_html
         tables += "</br>"
     return tables
-    # ic(tables)
-
 # %%
 def Get_Amz_Description(tables, Str_desc):
     description = tables + "</br></br>" + Str_desc
@@ -249,18 +231,88 @@ def Get_color_image(soup):
         image = "https:" + doc.img.get("src")
         big_image = image.split("_thumb")[0] + ".jpg"
         other_images.append(big_image)
-    # ic(other_images)
     return other_images
-# ic(Get_color_image(soup))
 # %%
+def Get_result_row(soup, color):
+    soup = BeautifulSoup(brower.page_source, "lxml")
+    sku = Get_sku(soup)
+    brand = Get_brand(soup)
+    price = Get_price(soup)
+    new_title = Get_new_title(soup, brand)
+    size_names = Get_size_name(soup)
+    desc = Get_desc(soup)
+    bullet_point = Get_Desc_str(desc)
+    amz_desc = Get_Amz_desc(desc)
+    size_chart = Get_Size_Chart(soup)
+    description = Get_Amz_Description(size_chart, amz_desc)
+    images_url = Get_color_image(soup)
+    # 循环尺码,进行sku拼接, sku-s, sku-m
+    def Get_image_link(n):
+        n_image =""
+        try:
+            n_image = images_url[n]
+        except Exception as e:
+            n_image = ""
+        return n_image
+    shein_datas = []
+
+    # 子产品
+    for item in size_names:
+        n_size_name = item
+        n_sku = sku + "-" + str(item)
+        n_brand = brand
+        n_price = price
+        n_title = new_title
+        n_bullet_point = bullet_point
+        n_description = description
+        n_image0 = Get_image_link(0)
+        n_image1 = Get_image_link(1)
+        n_image2 = Get_image_link(2)
+        n_image3 = Get_image_link(3)
+        n_image4 = Get_image_link(4)
+        n_image5 = Get_image_link(5)
+        n_image6 = Get_image_link(6)
+        n_image7 = Get_image_link(7)
+        data = [n_sku, n_brand, n_price, n_title, n_bullet_point, n_description, n_size_name, color, n_image0,n_image1, n_image2
+                , n_image3,n_image4, n_image5, n_image6, n_image7]
+        shein_datas.append(data)
+    # ic(shein_datas)
+    return shein_datas
+
+
+
+
+# %%
+# 引入改变语言模块
+from Change_language import *
+
+url = "https://jp.shein.com/Dazy-Less-Solid-Drop-Shoulder-Sweater-p-4634412-cat-1734.html?scici=WomenHomePage~~ON_Banner,CN_category,HZ_Knitwear,HI_hotZoneuadal3wddc8~~6_1~~real_2216~~~~"
+path = "/home/dav/Downloads/chromedriver" 
+brower = webdriver.Chrome(executable_path=path)
+# 改变语言为日语
+change_Janpn = Change_language()
+change_Janpn.load(brower, url)
+datas = []
+# 找到所有颜色
 elements = brower.find_elements_by_xpath("/html/body/div[1]/div[1]/div/div[1]/div/div[2]/div[2]/div/div[2]/div[1]/div[2]/div")
-for i in range(1, len(elements)):
+for i in range(len(elements)):
     elements[i].click()
     time.sleep(10)
     # js重新加载网页,网页内容会变化,如果还用之前的soup数据不会变化
     soups = BeautifulSoup(brower.page_source, "lxml")
-    ic(Get_color_image(soups))
-    ic(Get_sku(soups))
+    color = soups.find("span", {"class" : "color-999"}).text.strip()
+    data = Get_result_row(soups,color)
+    # datas.append(data)
+    datas += data
+
+# %%
+import csv
+with open("reslut.csv", "w", encoding="utf-8") as f:
+    writer = csv.writer(f)
+    writer.writerow(["sku", "brand", "price", "title", "bullet_point", "description","size_name","color","image1", "image2", "image3", "image4", "image5", "image6", "image7", "image8"])
+    for row in datas:
+        writer.writerow(row)
+f.close()
 
 # %%
 # TODO: 重构数据  <26-12-21, yuzhe> #
